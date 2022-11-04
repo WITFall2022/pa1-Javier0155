623ec24f88b2ed82c04b2998bbdd2db7ee1716dc
307504 <307504@664-161458-LAB.schools.mybps.org>
2022-11-04 11:40:43 -0400

    I completed Assignments Problem 1 A and Problem 1 B

--- src/edu/wit/cs/comp1000/PA1b.java
+++ src/edu/wit/cs/comp1000/PA1b.java
@@ -1,11 +1,35 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA1b {
 
 	// TODO: document this function
 	public static void main(String[] args) {
-		// TODO: write your code here
+		
+			
+			Scanner input=new Scanner(System.in);
+			
+			System.out.println("enter number of inches in whole numbers");
+			
+			int inches = input.nextInt(); 
+			
+			int yards= inches /36;
+			int inchesf = inches % 36; 
+			int feet= inchesf /12;
+			int inchest= inchesf % 12; 
+			
+			System.out.println("The total number of yards is" + yards);
+		    System.out.println("The total number of feet is"  + feet); 
+		    System.out.println("The total number of inches"   + inchest); 
+			}
+			
+			
+
+		
+
+
 	}
 
-}
+
